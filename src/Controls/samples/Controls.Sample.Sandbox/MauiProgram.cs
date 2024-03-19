f7fb3d1da95576bed3926c1e2888f458036772e8
tj-devel709 <tj.devel709@gmail.com>
2024-03-19 14:51:34 -0500

    starting

--- src/Controls/samples/Controls.Sample.Sandbox/MauiProgram.cs
+++ src/Controls/samples/Controls.Sample.Sandbox/MauiProgram.cs
@@ -2,6 +2,7 @@
 using Microsoft.Maui.Controls;
 using Microsoft.Maui.Controls.Hosting;
 using Microsoft.Maui.Hosting;
+using Microsoft.Maui.Controls.PlatformConfiguration.AndroidSpecific;
 
 namespace Maui.Controls.Sample
 {
@@ -17,12 +18,13 @@ namespace Maui.Controls.Sample
 				.Build();
 	}
 
-	class App : Application
+	class App : Microsoft.Maui.Controls.Application
 	{
 		protected override Window CreateWindow(IActivationState? activationState)
 		{
 			// To test shell scenarios, change this to true
 			bool useShell = false;
+			On<Microsoft.Maui.Controls.PlatformConfiguration.Android>().UseWindowSoftInputModeAdjust(WindowSoftInputModeAdjust.Resize);
 
 			if (!useShell)
 			{
