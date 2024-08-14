6ea6b2970f98f21c10bf59cc9527b211761af70e
Rui Marinho <me@ruimarinho.net>
2024-08-14 15:40:51 +0100

    [tests] Enable new handlers

--- src/Controls/tests/TestCases.HostApp/MauiProgram.cs
+++ src/Controls/tests/TestCases.HostApp/MauiProgram.cs
@@ -24,6 +24,14 @@ namespace Maui.Controls.Sample
 					fonts.AddFont("ionicons.ttf", "Ion");
 				})
 				.Issue21109AddMappers();
+#if IOS || MACCATALYST
+
+			appBuilder.ConfigureMauiHandlers(handlers =>
+				{
+					handlers.AddHandler<Microsoft.Maui.Controls.CollectionView, Microsoft.Maui.Controls.Handlers.Items2.CollectionViewHandler2>();
+					handlers.AddHandler<Microsoft.Maui.Controls.CarouselView, Microsoft.Maui.Controls.Handlers.Items2.CarouselViewHandler2>();
+				});
+#endif
 
 			appBuilder.Services.AddTransient<TransientPage>();
 			appBuilder.Services.AddScoped<ScopedPage>();
